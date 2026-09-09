40830b696c1ca5a0fe3f0a938cee15d990f8fd9e
LPcomix123 <louisplayphone123@gmail.com>
2026-09-09 21:55:12 +0100

    added magic 8 ball and fixed almost all if not all bugs in the magic 8 ball feature, and thats all im doin for today, cya

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ while isactive == 1:
         slp(1)
         clear()
         isactive = 0
-    elif mode == "4" or mode == "5" or mode == "7": # Feature Under Development
+    elif mode == "5" or mode == "7": # Feature Under Development
         clear()
         qtype("Feature " + mode + " is currently under development. Please check back later.")
         slp(1)
@@ -334,6 +334,55 @@ while isactive == 1:
         print(num)
         slp(2)
         mode = "nmenu"
+    elif mode == "4": # Magic 8 Ball
+        answers = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        ]
+        clear()
+        type("🎱  Magic 8 Ball")
+        print(" ")
+        print(" ")
+        qtype("Ask a question or type 'exit' to return to the menu: ")
+        question = input()
+        if question == "exit":
+            mode = "nmenu"
+            continue
+        else:
+            clear()
+            print("🎱  Magic 8 Ball")
+            print(" ")
+            print(" ")
+            qtype("You asked: " + question)
+            slp(1)
+            clear()
+            print("🎱  Magic 8 Ball")
+            print(" ")
+            print(" ")
+            qtype("And The Magic 8 Ball says: ")
+            slp(1)
+            sstype("...")
+            type(random.choice(answers))
+            slp(2)
+            mode = "nmenu"
     elif mode == "8": # About Section
         clear()
         qtype("About this program:")
